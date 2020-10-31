5541bc6a1631da491ed5cd95b3cc5c177a5d4e88
glitch-in-the-herring <70984234+glitch-in-the-herring@users.noreply.github.com>
2020-10-31 07:06:52 +0700

    who made these typos? it looks like somebody made this with insomnia

--- utils/database.py
+++ utils/database.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 conn = sqlite3.connect("counter.db")
 c = conn.cursor()
 
-class Dabatase(commands.Cog):
+class Database(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 
